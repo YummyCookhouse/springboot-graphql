d1bf2cb965a0e40d3bbf3429051ccfd794294ecc
Cheng Li <cheng.li@myob.com>
2019-02-14 09:30:15 +1100

    Remove unused import and bump up graphql tools and starter version

--- src/main/java/com/stardust/graphql/demo/resolvers/BirdResolver.java
+++ src/main/java/com/stardust/graphql/demo/resolvers/BirdResolver.java
@@ -2,13 +2,10 @@ package com.stardust.graphql.demo.resolvers;
 
 import com.coxautodev.graphql.tools.GraphQLResolver;
 import com.stardust.graphql.demo.entities.Bird;
-import com.stardust.graphql.demo.entities.Dog;
 import com.stardust.graphql.demo.entities.Owner;
-import com.stardust.graphql.demo.entities.Pet;
 import com.stardust.graphql.demo.services.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 
 @Component
 public class BirdResolver implements GraphQLResolver<Bird> {
